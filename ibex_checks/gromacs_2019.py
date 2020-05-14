4e28cc7889f50486c65ae795e2c134502e38b6bc
ismailiy <iten.ismail@kaust.edu.sa>
2020-05-14 17:03:47 +0300

    gromacs small, medium, large and multinode tests added

--- ibex_checks/gromacs_2019.py
+++ ibex_checks/gromacs_2019.py
@@ -5,11 +5,11 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 # as a Run a pre-compiled executable. Thus path to source code and method of compiling will be required.
-@rfm.simple_test
+@rfm.parameterized_test(['small'],['medium'],['large'],['multinode'])
 class gromacs_check(rfm.RunOnlyRegressionTest):
-    def __init__(self,**kwargs):
+    def __init__(self,variant):
         # Description of test. Should be short but telling
-        self.descr = 'GROMACS 2019 Run Only tests'
+        self.descr = 'GROMACS 2019 SP Run Only tests'
         self.valid_systems = ['ibex:batch']
         self.valid_prog_environs = ['intel']
         
@@ -25,10 +25,21 @@ class gromacs_check(rfm.RunOnlyRegressionTest):
 
         # you can define your job configuration here: 
         #(Check https://reframe-hpc.readthedocs.io/en/latest/reference.html for all possible options)
+
         self.time_limit = (0,30,0)  #is a tuple in the format (H,M,S)
-        self.num_tasks=8
-        self.num_tasks_per_node=8
-        
+        if variant == "small":
+          self.num_tasks=8
+          self.num_tasks_per_node=8
+        elif variant == "medium":  
+          self.num_tasks=16
+          self.num_tasks_per_node=16 
+        elif variant == "large":
+          self.num_tasks=40
+          self.num_tasks_per_node=40
+        else:
+          self.num_tasks=128
+          self.num_tasks_per_node=40
+
         # List of modules, if any, to be loaded in the jobscript
         self.modules=['gromacs/2019.4/openmpi-2.1.1-intel-2018-sp']
         # method pre_run adds any commands you wish to run before srun in your jobscript. 
@@ -42,7 +53,7 @@ class gromacs_check(rfm.RunOnlyRegressionTest):
         
         # perf_pattern is a method to capture the performance metric from the output file self.stdout (or any other filename with relative path)
         self.perf_patterns = {       
-                    'ns_day': sn.extractsingle(r'^Performance:\s+(?P<ns_day>\S+)', 'rfm_gromacs_check_job.err', 'ns_day', float)
+                    variant: sn.extractsingle(r'^Performance:\s+(?P<ns_day>\S+)', self.stderr, 'ns_day', float)
                                }
         
         # the captured metric in perf_patterns needs to be compared with a reference 
@@ -51,14 +62,18 @@ class gromacs_check(rfm.RunOnlyRegressionTest):
         # For example (0.027,-0.005,0.005) implies that the performance quantity should 
         # be 0.37 with tolerance of -10% or +10%
         self.reference = {
-                            'ibex' : {      'ns_day': (28, -0.1, 0.1)
-                                            },
+                            'ibex' : {    
+                               'small': (28, -0.1, 0.1),
+                               'medium': (49, -0.1, 0.1),
+                               'large': (98, -0.1, 0.1),
+                               'multinode': (216, -0.1, 0.1),
+                 
+                                     },
                             }
         
         
         # tags are useful to filter tests when not all but specific the tests are suppose to run
-        self.tags = {'gromacs','acceptance'}
-
+        self.tags = {'gromacs','gromacs_'+variant,'acceptance'}
         
         # initials or email of the maintainer    
         #self.maintainers = ['MS']
