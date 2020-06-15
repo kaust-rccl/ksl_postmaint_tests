de32d1e79618bad2923112447fadf50d73e69c52
ismailiy <iten.ismail@kaust.edu.sa>
2020-06-15 14:41:22 +0300

    license tests

---
+++ resourcesdir/license/codeexample.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+    number1=20;
+    number2=50;
+    sum = number1 + number2;      
+    printf("%d", sum);
+     return 0;
+           }
