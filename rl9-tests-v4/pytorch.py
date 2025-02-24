952d330215229ac768ffda48c2fbb1280bd90e29
mgharawy <141732010+mgharawy@users.noreply.github.com>
2025-02-24 11:29:03 +0200

    Added pytorch multinode test for A100_4

--- rl9-tests-v4/pytorch.py
+++ rl9-tests-v4/pytorch.py
@@ -9,32 +9,33 @@ class pytorch_test(rfm.RunOnlyRegressionTest):
       sourcesdir= '../src/pytorch/a100'
       valid_systems = ['ibex:batch']
       valid_prog_environs = ['gpustack_builtin']
-      
+
       @run_before('sanity')
       def set_sanity_patterns(self):
           self.sanity_patterns = sn.assert_found(r'Epoch_time', self.stdout)
 
- 
+
       @run_before('performance')
       def set_perf_patterns(self):
           self.perf_patterns = {self.variant: sn.extractsingle(r'^[E]\w+[_]\w+[:]\s+(?P<Epoch_time>\d*\.\d+)',self.stdout, 'Epoch_time', float)}
-                
-                
+
+
 @rfm.simple_test
 class pytorch_a100_gpu(pytorch_test):
-      variant= parameter(['a100_8_singlenode', 'a100_4_singlenode','a100_8_4GPUS_singlenode', 'a100_8_multinode'])
+      variant= parameter(['a100_8_singlenode', 'a100_4_singlenode','a100_4_multinode' ,'a100_8_4GPUS_singlenode', 'a100_8_multinode'])
       valid_systems = ['ibex:batch']
-      time_limit = "3h"  
+      time_limit = "3h"
       reference = {
                         'ibex' : {
                                 'a100_8_singlenode' : (1100.00,None,+0.1,None),
                                 'a100_4_singlenode' : (2200.00,None,+0.1,None),
                                 'a100_8_4GPUS_singlenode' : (2100.00,None,+0.1,None),
-                                'a100_8_multinode'  : (1100,None,+10,None)
-                               
+                                'a100_8_multinode'  : (1100,None,+10,None),
+                                'a100_4_multinode' :(1100,None,+10,None)
+
                         }
                 }
-      
+
       @run_after('init')
       def setting_variables(self):
           self.tags |= {'a100' , self.variant}
@@ -48,9 +49,9 @@ class pytorch_a100_gpu(pytorch_test):
                               'export main_exe="./train_resnet50.py"','batch_size=256',
                               'epochs=5', 'export workers=${SLURM_CPUS_PER_TASK}','module list',
                               'export cmd="python3 ${main_exe} --epochs ${epochs} --batch-size ${batch_size} --num_workers=$workers --root-dir=${DATA_DIR} --train-dir ${DATA_DIR}/train --val-dir ${DATA_DIR}/val ${NODE_LOCAL_STORAGE}"']
-          
+
           self.executable= 'time -p srun -u -n ${SLURM_NTASKS} -N ${SLURM_NNODES} -c ${SLURM_CPUS_PER_TASK} ${cmd} --log-dir=log.${SLURM_JOBID} --warmup-epochs=0.0'
-          
+
           if self.variant == 'a100_8_singlenode':
              self.num_tasks= 8
              self.num_cpus_per_task=5
@@ -71,8 +72,12 @@ class pytorch_a100_gpu(pytorch_test):
              self.num_cpus_per_task=5
              self.extra_resources = {'constraint': {'type': 'a100,8gpus'},'memory': {'size': '400G'}}
              #self.num_gpus_per_node=8
-      
-      
+          elif self.variant == 'a100_4_multinode':
+             self.num_tasks= 8
+             self.num_cpus_per_task=5
+             self.extra_resources = {'constraint': {'type': 'a100,4gpus'},'memory': {'size': '400G'}}
+
+
       @run_before('run')
       def set_job_options(self):
         if self.variant == 'a100_8_4GPUS_singlenode':
@@ -85,10 +90,14 @@ class pytorch_a100_gpu(pytorch_test):
           self.job.options = ['--gpus=16',
                               '--gpus-per-node=4',
                               ]
+        elif self.variant == 'a100_4_multinode':
+          self.job.options = ['--gpus=8',
+                              '--gpus-per-node=4',
+                              ]
 
-  
-     
-     
+
+
+
 @rfm.simple_test
 class pytorch_v100_gpu(pytorch_test):
       variant = parameter(['v100_8_singlenode' , 'v100_8_multinode','v100_1_multinode'])
@@ -99,7 +108,7 @@ class pytorch_v100_gpu(pytorch_test):
                                 'v100_8_multinode'  : (1100,None,+10,None)
                         }
                 }
-      
+
       @run_after('init')
       def setting_variables(self):
         self.tags |= {'v100' , self.variant}
@@ -115,17 +124,17 @@ class pytorch_v100_gpu(pytorch_test):
                              'epochs=5', 'export workers=${SLURM_CPUS_PER_TASK}','module list',
                              'export cmd="python3 ${main_exe} --epochs ${epochs} --batch-size ${batch_size} --num_workers=$workers --root-dir=${DATA_DIR} --train-dir ${DATA_DIR}/train --val-dir ${DATA_DIR}/val ${NODE_LOCAL_STORAGE}"']
 
-        self.executable= 'time -p srun -u -n ${SLURM_NTASKS} -N ${SLURM_NNODES} -c ${SLURM_CPUS_PER_TASK} ${cmd} --log-dir=log.${SLURM_JOBID} --warmup-epochs=0.0'  
-        
-        if self.variant == 'v100_8_singlenode': 
+        self.executable= 'time -p srun -u -n ${SLURM_NTASKS} -N ${SLURM_NNODES} -c ${SLURM_CPUS_PER_TASK} ${cmd} --log-dir=log.${SLURM_JOBID} --warmup-epochs=0.0'
+
+        if self.variant == 'v100_8_singlenode':
            self.num_tasks=8
            self.num_cpus_per_task=5
-           
+
 
         elif  self.variant == 'v100_8_multinode':
            self.num_tasks= 16
            self.num_cpus_per_task=6
-               
+
 
         elif  self.variant == 'v100_1_multinode':
            self.num_tasks= 2
@@ -137,7 +146,7 @@ class pytorch_v100_gpu(pytorch_test):
            self.job.options = ['--gpus=8',
                               '--gpus-per-node=8'
                              ]
-                              
+
         elif self.variant == 'v100_8_multinode':
            self.job.options = ['--gpus=16',
                               '--gpus-per-node=4'
