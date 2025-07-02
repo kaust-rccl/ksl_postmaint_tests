52700993b211f7f2db673d8b023ceedb903ad8ed
mgharawy <141732010+mgharawy@users.noreply.github.com>
2025-07-02 15:48:54 +0300

    Update slurm_check.py

--- rl9-tests-v4/slurm_check.py
+++ rl9-tests-v4/slurm_check.py
@@ -24,7 +24,7 @@ class slurm_check(rfm.RunOnlyRegressionTest):
             self.sanity_patterns =sn.assert_found(r'JobID           JobName  Partition    Account  AllocCPUS      State ExitCode ',self.stdout)
 
         self.maintainers = ['mohsin.shaikh@kaust.edu.sa']
-        self.tags = {'slurm','acceptance','cpu'}
+        self.tags = {'slurm','acceptance','cpu','singlenode'}
 
 
 
