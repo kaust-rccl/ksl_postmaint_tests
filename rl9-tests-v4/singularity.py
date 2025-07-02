c194ed2a85a40691a89c27795ec76e925692bc35
mgharawy <141732010+mgharawy@users.noreply.github.com>
2025-07-02 15:48:19 +0300

    Update singularity.py

--- rl9-tests-v4/singularity.py
+++ rl9-tests-v4/singularity.py
@@ -38,4 +38,4 @@ class singularity_checks(rfm.RunOnlyRegressionTest):
                     },
             }        
         self.maintainers = ['mohsin.shaikh@kaust.edu.sa']
-        self.tags = {'cpu','acceptance','singularity'}
+        self.tags = {'cpu','acceptance','singularity','singlenode'}
