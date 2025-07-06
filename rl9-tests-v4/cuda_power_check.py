c43b1c37ec536350077e335a13491d4b1abb1205
Mohamed  Elgharawy <elghm0a@login510-27.ibex.kaust.edu.sa>
2025-07-06 04:33:30 +0300

    added power limit test

---
+++ rl9-tests-v4/cuda_power_check.py
@@ -0,0 +1,63 @@
+import os
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+@rfm.simple_test
+class Cuda_power_checks(rfm.RunOnlyRegressionTest):
+      variant= parameter(['v100_4','v100_8', 'p100','rtx2080ti','a100_4','a100_8'])
+
+      @run_after('init')
+      def setting_variables(self):
+        self.descr = 'CUDA Power query'
+        self.constraint = self.variant
+        self.tags = {'gpu',self.variant,'acceptance','power_query','cuda','singlenode'}
+        # Environment settings
+        self.valid_systems = ['ibex:batch']
+
+        self.valid_prog_environs = ['gpustack_cuda']
+        self.time_limit = '10m'
+        if self.variant == 'v100_8' or self.variant == 'rtx2080ti' or self.variant == 'a100_8' :
+           self.num_gpus_per_node=8
+        else:
+           self.num_gpus_per_node=4
+
+        # Resource and runtime settings
+       
+        self.num_tasks=1
+
+        # In the run phase invoke the executable name as below
+        self.prerun_cmds = ['./env.sh']
+        self.executable='bash -c \'nvidia-smi -q -d POWER | grep -i "Current Power Limit" | grep -v "N/A"\''
+        if self.variant == 'v100_4' or self.variant == 'v100_8':
+           self.extra_resources = {'constraint': {'type': 'v100'}}
+        elif self.variant == 'p100':
+           self.extra_resources = {'constraint': {'type': 'p100'}}
+        elif self.variant == 'rtx2080ti':
+           self.extra_resources = {'constraint': {'type': 'rtx2080ti'}}
+        elif self.variant == 'a100_4':
+           self.extra_resources = {'constraint': {'type': '4gpus,a100'}}
+        elif self.variant == 'a100_8':
+           self.extra_resources = {'constraint': {'type': 'a100'}}
+
+
+
+        #Validation
+        self.sanity_patterns = sn.assert_found (r'Current Power Limit' , self.stdout)
+        # Performance check
+        power_limits = sn.extractall(r'Current Power Limit\s+:\s+(?P<watts>\d+\.\d+)\sW', self.stdout, 'watts', float) 
+        min_power = sn.min(power_limits)
+        self.perf_patterns = {self.variant : min_power }
+        self.reference = {
+                            'ibex' : {      'p100': (250.0,None,None,'watts'),
+                                            'v100_4': (300.0,None,None,'watts'),
+                                            'v100_8': (300.0,None,None,'watts'),
+                                            'rtx2080ti': (250.0,None,None,'watts'),
+                                            'a100_8': (400.0,None,None,'watts'),
+                                            'a100_4': (400.0,None,None,'watts')
+
+                                            },
+                            }
+
+
+        self.maintainers = ['mohamed.elgharawy@kaust.edu.sa']
+        
